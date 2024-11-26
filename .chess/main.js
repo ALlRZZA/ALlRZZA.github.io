e5e6c994e03d6ff501960ed192a80a02186c501a
alirzza9 <119541367+alirzza9@users.noreply.github.com>
2024-11-26 20:42:19 +0330

    Update main.js

--- .chess/main.js
+++ .chess/main.js
@@ -1,1 +1,33 @@
+const board = Chessboard('board', {
+    draggable: true,
+    dropOffBoard: 'trash',
+    onDrop: handleMove
+});
+
+const game = new Chess();
+
+function handleMove(source, target) {
+    const move = game.move({
+        from: source,
+        to: target,
+        promotion: 'q' // ارتقای پیاده به وزیر
+    });
+
+    if (!move) {
+        // اگر حرکت غیرقانونی باشه (برای سیاه‌ها)
+        alert('حرکت غیرمجاز!');
+        return 'snapback';
+    }
+
+    if (game.turn() === 'w') {
+        // قوانین رو برای سفید نادیده بگیر
+        game.undo();
+        board.position(game.fen());
+    }
+}
+
+function invitePlayer() {
+    const url = window.location.href;
+    prompt('لینک رو برای دوستت بفرست:', url);
+}
 
